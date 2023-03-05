e11762aa15b7c513ec89e7caa387166db2eb4ef6
David Fineboym <kurbanov0409@gmail.com>
2023-03-05 23:04:41 +0200

    Upgrade NuGets

--- LoggingDecoratorGenerator.Tests/GeneratorSnapshotTests.GeneratesCorrectly#DecorateAttribute.g.received.cs
+++
@@ -1,9 +0,0 @@
-﻿//HintName: DecorateAttribute.g.cs
-
-namespace Fineboym.Logging.Generator
-{
-    [System.AttributeUsage(System.AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
-    public class DecorateAttribute : System.Attribute
-    {
-    }
-}
--- LoggingDecoratorGenerator.Tests/GeneratorSnapshotTests.GeneratesCorrectly#DecorateWithLoggerAttribute.g.received.cs
+++
@@ -1,9 +0,0 @@
-﻿//HintName: DecorateWithLoggerAttribute.g.cs
-
-namespace Fineboym.Logging.Generator
-{
-    [System.AttributeUsage(System.AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
-    internal class DecorateWithLoggerAttribute : System.Attribute
-    {
-    }
-}
