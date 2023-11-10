5cd2de47b68a2da9155fcd1cfd6c0abcfb34f9d7
David Fineboym <kurbanov0409@gmail.com>
2023-11-10 18:06:49 +0200

    Use new FakeLogger from Microsoft for tests

--- LoggingDecoratorGenerator.IntegrationTests/BeginScopeContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LoggingDecoratorGenerator.IntegrationTests;
-
-public class BeginScopeContext
-{
-    public object Scope { get; set; }
-
-    public string LoggerName { get; set; }
-}
--- LoggingDecoratorGenerator.IntegrationTests/InformationLevelInterfaceTests.cs
+++ LoggingDecoratorGenerator.IntegrationTests/InformationLevelInterfaceTests.cs
@@ -1,20 +1,21 @@
 using FakeItEasy;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Testing;
 using OtherFolder.OtherSubFolder;
+using System.Globalization;
 
 namespace LoggingDecoratorGenerator.IntegrationTests;
 
 public class InformationLevelInterfaceTests
 {
-    private readonly TestSink _testSink;
+    private readonly FakeLogCollector _collector;
     private readonly IInformationLevelInterface _fakeService;
     private readonly InformationLevelInterfaceLoggingDecorator _decorator;
 
     public InformationLevelInterfaceTests()
     {
-        _testSink = new();
-        TestLoggerFactory testLoggerFactory = new(_testSink, enabled: true);
-        ILogger<IInformationLevelInterface> logger = new Logger<IInformationLevelInterface>(testLoggerFactory);
+        _collector = new();
+        FakeLogger<IInformationLevelInterface> logger = new(_collector);
         _fakeService = A.Fake<IInformationLevelInterface>();
         _decorator = new InformationLevelInterfaceLoggingDecorator(logger, _fakeService);
     }
@@ -35,40 +36,40 @@ public class InformationLevelInterfaceTests
         Assert.Equal(expected: expectedReturnValue, actual: actualReturn);
         A.CallTo(() => _fakeService.MethodWithoutAttribute(x, y)).MustHaveHappenedOnceExactly();
 
-        Assert.Equal(2, _testSink.Writes.Count);
+        Assert.Equal(2, _collector.Count);
 
-        WriteContext firstWrite = _testSink.Writes.First();
-        Assert.Equal(1514124652, firstWrite.EventId.Id);
-        Assert.Equal("MethodWithoutAttribute", firstWrite.EventId.Name);
-        Assert.Equal(LogLevel.Information, firstWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.LoggerName);
+        IReadOnlyList<FakeLogRecord> writes = _collector.GetSnapshot();
+
+        FakeLogRecord firstWrite = writes[0];
+        Assert.Equal(1514124652, firstWrite.Id.Id);
+        Assert.Equal("MethodWithoutAttribute", firstWrite.Id.Name);
+        Assert.Equal(LogLevel.Information, firstWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.Category);
         Assert.Equal("Entering MethodWithoutAttribute with parameters: x = 42, y = 43", firstWrite.Message);
         Assert.Null(firstWrite.Exception);
-        Assert.Null(firstWrite.Scope);
-        var beforeWriteState = (IReadOnlyList<KeyValuePair<string, object>>)firstWrite.State;
+        Assert.Empty(firstWrite.Scopes);
         KeyValuePair<string, object>[] expectedBeforeWriteState = new[]
         {
             new KeyValuePair<string, object>("x", x),
             new KeyValuePair<string, object>("y", y),
             new KeyValuePair<string, object>("{OriginalFormat}", "Entering MethodWithoutAttribute with parameters: x = {x}, y = {y}"),
         };
-        LogValuesAssert.Contains(expectedBeforeWriteState, beforeWriteState);
+        Assert.Equivalent(expectedBeforeWriteState, firstWrite.StructuredState, strict: true);
 
-        WriteContext lastWrite = _testSink.Writes.Last();
-        Assert.Equal(1514124652, lastWrite.EventId.Id);
-        Assert.Equal("MethodWithoutAttribute", lastWrite.EventId.Name);
-        Assert.Equal(LogLevel.Information, lastWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.LoggerName);
+        FakeLogRecord lastWrite = writes[1];
+        Assert.Equal(1514124652, lastWrite.Id.Id);
+        Assert.Equal("MethodWithoutAttribute", lastWrite.Id.Name);
+        Assert.Equal(LogLevel.Information, lastWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.Category);
         Assert.Equal("Method MethodWithoutAttribute returned. Result = 42.43", lastWrite.Message);
         Assert.Null(lastWrite.Exception);
-        Assert.Null(lastWrite.Scope);
-        var afterWriteState = (IReadOnlyList<KeyValuePair<string, object>>)lastWrite.State;
+        Assert.Empty(lastWrite.Scopes);
         KeyValuePair<string, object>[] expectedAfterWriteState = new[]
         {
             new KeyValuePair<string, object>("result", expectedReturnValue),
             new KeyValuePair<string, object>("{OriginalFormat}", "Method MethodWithoutAttribute returned. Result = {result}"),
         };
-        LogValuesAssert.Contains(expectedAfterWriteState, afterWriteState);
+        Assert.Equivalent(expectedAfterWriteState, lastWrite.StructuredState, strict: true);
     }
 
     [Fact]
@@ -87,40 +88,40 @@ public class InformationLevelInterfaceTests
         Assert.Equal(expected: expectedReturnValue, actual: actualReturn);
         A.CallTo(() => _fakeService.MethodWithAttribute(firstInput, secondInput)).MustHaveHappenedOnceExactly();
 
-        Assert.Equal(2, _testSink.Writes.Count);
+        Assert.Equal(2, _collector.Count);
 
-        WriteContext firstWrite = _testSink.Writes.First();
-        Assert.Equal(100, firstWrite.EventId.Id);
-        Assert.Equal("SomePersonEventName", firstWrite.EventId.Name);
-        Assert.Equal(LogLevel.Debug, firstWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.LoggerName);
+        IReadOnlyList<FakeLogRecord> writes = _collector.GetSnapshot();
+
+        var firstWrite = writes[0];
+        Assert.Equal(100, firstWrite.Id.Id);
+        Assert.Equal("SomePersonEventName", firstWrite.Id.Name);
+        Assert.Equal(LogLevel.Debug, firstWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.Category);
         Assert.Equal("Entering MethodWithAttribute with parameters: person = Person { Name = foo, Age = 30 }, someNumber = 33", firstWrite.Message);
         Assert.Null(firstWrite.Exception);
-        Assert.Null(firstWrite.Scope);
-        var beforeWriteState = (IReadOnlyList<KeyValuePair<string, object>>)firstWrite.State;
-        KeyValuePair<string, object>[] expectedBeforeWriteState = new[]
+        Assert.Empty(firstWrite.Scopes);
+        KeyValuePair<string, string>[] expectedBeforeWriteState = new[]
         {
-            new KeyValuePair<string, object>("person", firstInput),
-            new KeyValuePair<string, object>("someNumber", secondInput),
-            new KeyValuePair<string, object>("{OriginalFormat}", "Entering MethodWithAttribute with parameters: person = {person}, someNumber = {someNumber}"),
+            new KeyValuePair<string, string>("person", firstInput.ToString()),
+            new KeyValuePair<string, string>("someNumber", secondInput.ToString()),
+            new KeyValuePair<string, string>("{OriginalFormat}", "Entering MethodWithAttribute with parameters: person = {person}, someNumber = {someNumber}"),
         };
-        LogValuesAssert.Contains(expectedBeforeWriteState, beforeWriteState);
+        Assert.Equivalent(expectedBeforeWriteState, firstWrite.StructuredState, strict: true);
 
-        WriteContext lastWrite = _testSink.Writes.Last();
-        Assert.Equal(100, lastWrite.EventId.Id);
-        Assert.Equal("SomePersonEventName", lastWrite.EventId.Name);
-        Assert.Equal(LogLevel.Debug, lastWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.LoggerName);
+        var lastWrite = writes[1];
+        Assert.Equal(100, lastWrite.Id.Id);
+        Assert.Equal("SomePersonEventName", lastWrite.Id.Name);
+        Assert.Equal(LogLevel.Debug, lastWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.Category);
         Assert.Equal("Method MethodWithAttribute returned. Result = Person { Name = bar, Age = 42 }", lastWrite.Message);
         Assert.Null(lastWrite.Exception);
-        Assert.Null(lastWrite.Scope);
-        var afterWriteState = (IReadOnlyList<KeyValuePair<string, object>>)lastWrite.State;
-        KeyValuePair<string, object>[] expectedAfterWriteState = new[]
+        Assert.Empty(lastWrite.Scopes);
+        KeyValuePair<string, string>[] expectedAfterWriteState = new[]
         {
-            new KeyValuePair<string, object>("result", expectedReturnValue),
-            new KeyValuePair<string, object>("{OriginalFormat}", "Method MethodWithAttribute returned. Result = {result}"),
+            new KeyValuePair<string, string>("result", expectedReturnValue.ToString()),
+            new KeyValuePair<string, string>("{OriginalFormat}", "Method MethodWithAttribute returned. Result = {result}"),
         };
-        LogValuesAssert.Contains(expectedAfterWriteState, afterWriteState);
+        Assert.Equivalent(expectedAfterWriteState, lastWrite.StructuredState, strict: true);
     }
 
     [Fact]
@@ -156,41 +157,43 @@ public class InformationLevelInterfaceTests
         Assert.Equal(expected: expectedReturnValue, actual: actualReturn);
         A.CallTo(() => _fakeService.MethodWithMeasuredDurationAsync(inputParam)).MustHaveHappenedOnceExactly();
 
-        Assert.Equal(2, _testSink.Writes.Count);
+        Assert.Equal(2, _collector.Count);
 
-        WriteContext firstWrite = _testSink.Writes.First();
-        Assert.Equal(1711224704, firstWrite.EventId.Id);
-        Assert.Equal("MethodWithMeasuredDurationAsync", firstWrite.EventId.Name);
-        Assert.Equal(LogLevel.Information, firstWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.LoggerName);
+        IReadOnlyList<FakeLogRecord> writes = _collector.GetSnapshot();
+
+        var firstWrite = writes[0];
+        Assert.Equal(1711224704, firstWrite.Id.Id);
+        Assert.Equal("MethodWithMeasuredDurationAsync", firstWrite.Id.Name);
+        Assert.Equal(LogLevel.Information, firstWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.Category);
         Assert.Equal("Entering MethodWithMeasuredDurationAsync with parameters: someDate = 09/28/0003", firstWrite.Message);
         Assert.Null(firstWrite.Exception);
-        Assert.Null(firstWrite.Scope);
-        var beforeWriteState = (IReadOnlyList<KeyValuePair<string, object>>)firstWrite.State;
-        KeyValuePair<string, object>[] expectedBeforeWriteState = new[]
+        Assert.Empty(firstWrite.Scopes);
+        KeyValuePair<string, string>[] expectedBeforeWriteState = new[]
         {
-            new KeyValuePair<string, object>("someDate", inputParam),
-            new KeyValuePair<string, object>("{OriginalFormat}", "Entering MethodWithMeasuredDurationAsync with parameters: someDate = {someDate}"),
+            new KeyValuePair<string, string>("someDate", inputParam.ToString(DateTimeFormatInfo.InvariantInfo)),
+            new KeyValuePair<string, string>("{OriginalFormat}", "Entering MethodWithMeasuredDurationAsync with parameters: someDate = {someDate}"),
         };
-        LogValuesAssert.Contains(expectedBeforeWriteState, beforeWriteState);
+        Assert.Equivalent(expectedBeforeWriteState, firstWrite.StructuredState, strict: true);
 
-        WriteContext lastWrite = _testSink.Writes.Last();
-        Assert.Equal(1711224704, lastWrite.EventId.Id);
-        Assert.Equal("MethodWithMeasuredDurationAsync", lastWrite.EventId.Name);
-        Assert.Equal(LogLevel.Information, lastWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.LoggerName);
+        var lastWrite = writes[1];
+        Assert.Equal(1711224704, lastWrite.Id.Id);
+        Assert.Equal("MethodWithMeasuredDurationAsync", lastWrite.Id.Name);
+        Assert.Equal(LogLevel.Information, lastWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.Category);
         Assert.Null(lastWrite.Exception);
-        Assert.Null(lastWrite.Scope);
-        var afterWriteState = (IReadOnlyList<KeyValuePair<string, object>>)lastWrite.State;
-        KeyValuePair<string, object>[] expectedAfterWriteState = new[]
+        Assert.Empty(lastWrite.Scopes);
+        var afterWriteState = lastWrite.StructuredState;
+        KeyValuePair<string, string>[] expectedAfterWriteState = new[]
         {
-            new KeyValuePair<string, object>("result", expectedReturnValue),
-            new KeyValuePair<string, object>("{OriginalFormat}", "Method MethodWithMeasuredDurationAsync returned. Result = {result}. DurationInMilliseconds = {durationInMilliseconds}"),
+            new KeyValuePair<string, string>("result", expectedReturnValue.ToString()),
+            new KeyValuePair<string, string>("{OriginalFormat}", "Method MethodWithMeasuredDurationAsync returned. Result = {result}. DurationInMilliseconds = {durationInMilliseconds}"),
         };
-        LogValuesAssert.Contains(expectedAfterWriteState, afterWriteState);
-        double? duration = afterWriteState.SingleOrDefault(kvp => kvp.Key == "durationInMilliseconds").Value as double?;
-        Assert.True(duration > 0, $"Duration should be greater than 0, but was {duration}");
+        Assert.Equivalent(expectedAfterWriteState, afterWriteState);
+        string? durationString = afterWriteState!.SingleOrDefault(kvp => kvp.Key == "durationInMilliseconds").Value;
+        Assert.True(double.TryParse(durationString, out double duration));
         Assert.Equal($"Method MethodWithMeasuredDurationAsync returned. Result = Person {{ Name = bar, Age = 42 }}. DurationInMilliseconds = {duration}", lastWrite.Message);
+        Assert.InRange(duration, double.Epsilon, lastWrite.Timestamp.Subtract(firstWrite.Timestamp).TotalMilliseconds);
     }
 
     [Fact]
@@ -205,36 +208,36 @@ public class InformationLevelInterfaceTests
         Assert.Equal(expectedException, actualException);
         A.CallTo(() => _fakeService.MethodThrowsAndLogsExceptionAsync()).MustHaveHappenedOnceExactly();
 
-        Assert.Equal(2, _testSink.Writes.Count);
+        Assert.Equal(2, _collector.Count);
 
-        WriteContext firstWrite = _testSink.Writes.First();
-        Assert.Equal(777, firstWrite.EventId.Id);
-        Assert.Equal("MethodThrowsAndLogsExceptionAsync", firstWrite.EventId.Name);
-        Assert.Equal(LogLevel.Information, firstWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.LoggerName);
+        IReadOnlyList<FakeLogRecord> writes = _collector.GetSnapshot();
+
+        var firstWrite = writes[0];
+        Assert.Equal(777, firstWrite.Id.Id);
+        Assert.Equal("MethodThrowsAndLogsExceptionAsync", firstWrite.Id.Name);
+        Assert.Equal(LogLevel.Information, firstWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", firstWrite.Category);
         Assert.Equal("Entering MethodThrowsAndLogsExceptionAsync", firstWrite.Message);
         Assert.Null(firstWrite.Exception);
-        Assert.Null(firstWrite.Scope);
-        var beforeWriteState = (IReadOnlyList<KeyValuePair<string, object>>)firstWrite.State;
+        Assert.Empty(firstWrite.Scopes);
         KeyValuePair<string, object>[] expectedBeforeWriteState = new[]
         {
             new KeyValuePair<string, object>("{OriginalFormat}", "Entering MethodThrowsAndLogsExceptionAsync"),
         };
-        LogValuesAssert.Contains(expectedBeforeWriteState, beforeWriteState);
+        Assert.Equivalent(expectedBeforeWriteState, firstWrite.StructuredState, strict: true);
 
-        WriteContext lastWrite = _testSink.Writes.Last();
-        Assert.Equal(777, lastWrite.EventId.Id);
-        Assert.Equal("MethodThrowsAndLogsExceptionAsync", lastWrite.EventId.Name);
-        Assert.Equal(LogLevel.Error, lastWrite.LogLevel);
-        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.LoggerName);
+        var lastWrite = writes[1];
+        Assert.Equal(777, lastWrite.Id.Id);
+        Assert.Equal("MethodThrowsAndLogsExceptionAsync", lastWrite.Id.Name);
+        Assert.Equal(LogLevel.Error, lastWrite.Level);
+        Assert.Equal("LoggingDecoratorGenerator.IntegrationTests.IInformationLevelInterface", lastWrite.Category);
         Assert.Equal("MethodThrowsAndLogsExceptionAsync failed", lastWrite.Message);
         Assert.Equal(expectedException, lastWrite.Exception);
-        Assert.Null(lastWrite.Scope);
-        var afterWriteState = (IReadOnlyList<KeyValuePair<string, object>>)lastWrite.State;
+        Assert.Empty(lastWrite.Scopes);
         KeyValuePair<string, object>[] expectedAfterWriteState = new[]
         {
             new KeyValuePair<string, object>("{OriginalFormat}", "MethodThrowsAndLogsExceptionAsync failed"),
         };
-        LogValuesAssert.Contains(expectedAfterWriteState, afterWriteState);
+        Assert.Equivalent(expectedAfterWriteState, lastWrite.StructuredState, strict: true);
     }
 }
